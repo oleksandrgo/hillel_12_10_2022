87fa3547e646a1ccf1d7bccd48b495455facaa8b
latifundist <latifundist@gmail.com>
2023-02-08 22:21:59 +0200

    HW15

---
+++ HW15/java_qa_manual_lectures-main/src/com/hillel/qa/java/arithmetic_operators/homework/HomeWork1.java
@@ -0,0 +1,22 @@
+package com.hillel.qa.java.arithmetic_operators.homework;
+
+public class HomeWork1 {
+    /**
+     * <p style="font-size:12px">
+     * Написати програму, яка виводить на екран рік народження мого друга.
+     * </p>
+     *<br>
+     * <h2>Вимоги:</h2>
+     * <ul style="font-size:12px">
+     *     <li>Створіть змінну currentYear, яка буде містити поточний рік.</li>
+     *     <li>Створіть змінну friendYear, яка буде містити рік народження друга.</li>
+     *     <li>Вивести в консоль "Моєму другу (число) років".</li>
+     * </ul>
+     */
+    public static void main(String[] args) {
+        int currentYear = 2023;
+        int friendYear = 1978;
+        int age = currentYear - friendYear;
+        System.out.println("My friend Nik is " + age + " years old.");
+    }
+}
---
+++ HW15/java_qa_manual_lectures-main/src/com/hillel/qa/java/variables/homework/HomeWork1.java
@@ -0,0 +1,23 @@
+package com.hillel.qa.java.variables.homework;
+
+public class HomeWork1 {
+    /**
+     * <h2>Вимоги:</h2>
+     * <ul style="font-size:12px">
+     *     <li>У методі main оголоси чотири змінні типу int, double, String, boolean.</li>
+     *     <li>Змінні повинні мати імена year, height, countryName, isStudent.</li>
+     *     <li>Змінним одразу потрібно присвоїти значення.</li>
+     * </ul>
+     */
+    public static void main(String[] args) {
+        int year = 2023;
+        double height = 1.81;
+        String countryName = "Ukraine";
+        boolean isStudent = true;
+
+        System.out.println("Year: " + year);
+        System.out.println("Height: " + height);
+        System.out.println("Country Name: " + countryName);
+        System.out.println("Is student: " + isStudent);
+    }
+}
