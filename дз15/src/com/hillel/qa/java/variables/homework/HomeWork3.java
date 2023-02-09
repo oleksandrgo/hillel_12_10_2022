6271c9edf790ba6524c9aa95c422aa4c56a472e8
msushkom <msushkom@gmail.com>
2023-02-09 12:42:36 +0200

    Java

---
+++ дз15/src/com/hillel/qa/java/variables/homework/HomeWork3.java
@@ -0,0 +1,19 @@
+package com.hillel.qa.java.variables.homework;
+
+public class HomeWork3 {
+    /**
+     * <p style="font-size:12px">
+     * У методі main виводяться значення трьох рядків.
+     * Зміни код так, щоб замість значень рядків виводилась їх довжина.
+     * </p>
+     */
+    public static void main(String[] args) {
+        String emptyString = "";
+        System.out.println(emptyString.length());
+        String message = "Hello world";
+        System.out.println(message.length());
+        String data = "+2+2+22";
+        System.out.println(data.length());
+        /*System.out.println((emptyString + 2 + 2 + "22"));*/
+    }
+}
