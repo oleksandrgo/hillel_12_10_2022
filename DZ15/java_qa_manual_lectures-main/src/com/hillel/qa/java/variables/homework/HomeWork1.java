4d2552f1e8cc4ffa2a37b6408ce072ae8da827f5
Jendos91 <jendos.alex91@gmail.com>
2023-02-09 23:43:26 +0200

    DZ 15,16

---
+++ DZ15/java_qa_manual_lectures-main/src/com/hillel/qa/java/variables/homework/HomeWork1.java
@@ -0,0 +1,23 @@
+package com.hillel.qa.java.variables.homework;
+
+public class HomeWork1 {
+    /**
+     * <h2>Вимоги:</h2>
+     * <ul style="font-size:12px">
+     *     <li>У методі main оголоси чотири змінні типу int, double, String, boolean.</li>
+     *     <li>Змінні повинні мати імена year, height, countryName, isStudent.</li>
+     *     <li>Змінним одразу потрібно присвоїти значення.</li>
+     * </ul>
+     */
+    public static void main(String[] args) {
+
+        int year = 22;
+        double height = 38.5;
+        String countryName = "UA";
+        boolean isStudent = true;
+        System.out.println(year);
+        System.out.println(height);
+        System.out.println(countryName);
+        System.out.println(isStudent);
+    }
+}
